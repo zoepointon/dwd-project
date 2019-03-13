9efe8a5d769fdc5f4f121fee0634b4709cc350ef
Sarah Schöttler <sarah@schoettler.email>
2019-03-13 16:37:25 +0000

    minimal working version of events search

--- Website/FestivalSouvenirs V2/server.R
+++ Website/FestivalSouvenirs V2/server.R
@@ -21,27 +21,33 @@ shinyServer(function(input, output) {
         base <- 'https://api.edinburghfestivalcity.com/events'
 
 
-        events <- eventReactive({input$searchtitle
-                                input$searchartist
-                                input$searchvenue}, {
+        events <- eventReactive({input$date_from
+                                input$date_to
+                                input$title
+                                input$artist
+                                input$venue}, {
+                                        
+                date_from <- ifelse(input$date_from=='', '', 
+                                    paste0('&date_from=', curlEscape(input$date_from)))
+                date_to <- ifelse(input$date_to=='', '', 
+                                    paste0('&date_to=', curlEscape(input$date_to)))
+                artist <- ifelse(input$artist=='', '', 
+                                    paste0('&artist=', curlEscape(input$artist)))
+                venue <- ifelse(input$venue=='', '', 
+                                    paste0('&venue=', curlEscape(input$venue)))
+                title <- ifelse(input$title=='', '', 
+                                    paste0('&title=', curlEscape(input$title)))
 
-                query <- paste0('/events?year=2018&title=', curlEscape(input$searchtitle),
-                                '&artist=', curlEscape(input$searchartist),
-                                '&venue=', curlEscape(input$searchvenue),
+                query <- paste0('/events?year=2018',
+                                date_from, date_to, artist, venue, title,
                                 '&key=', api_key)
                 sig <- hmac(secret_key, query, algo='sha1', serialize = F)
-                my_url <- paste0('https://api.edinburghfestivalcity.com', query, '&signature=', sig)
-                mydata <- fromJSON(my_url)
+                myurl <- paste0('https://api.edinburghfestivalcity.com', query, '&signature=', sig)
+                mydata <- fromJSON(myurl)
                 mydata
         })
         
-        # output$nshows <- renderText({
-                # events()
-                #n <- nrow(events())
-                #if (is.null(n)) {n <- 0}
-                #paste('We found', n, 'shows matching your search term.')
-        # })
-        output$shows <- renderTable({
+        output$shows <- renderText({
                 events()$title
         })
 
